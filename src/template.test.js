38b7882e9b948d776a5c4e7c2adf1458c8dd434e
Brad Treloar <brad@treloardigital.com.au>
2021-04-21 13:50:45 +0930

    safely encode URLs

--- src/template.test.js
+++ src/template.test.js
@@ -4,9 +4,11 @@ const {
   Components,
   servicePriorityOptions,
   encodeObject,
+  decodeObject,
+  encodeMailUrl,
 } = require("./template");
 
-const expectTemplateOutputToMatch = function (expectedResult, result) {
+const expectTemplateOutputToMatch = function (result, expectedResult) {
   expect(result.replace(/\n/gi, "").replace(/ /gi, "")).toMatch(
     expectedResult.replace(/\n/gi, "").replace(/ /gi, "")
   );
@@ -21,29 +23,56 @@ describe("template tag function", () => {
     expect(result).toMatch(/foo/i);
   });
 
-  test("renders template with object value", () => {
+  test("encodes object value", () => {
     const value = {
       foo: "bar",
     };
     const result = template`
       ${value}
     `;
-    expect(result).toMatch(/{\"foo\":\"bar\"}/i);
+    expect(result).toMatch(/json:eyJmb28iOiJiYXIifQ==/i);
   });
+});
+
+test("encodes and decodes object", () => {
+  const value = { foo: "bar" };
+  const result = decodeObject(encodeObject(value));
+  expect(result.foo).toEqual(value.foo);
+});
+
+test("encodes mail URL", () => {
+  const result = encodeMailUrl(`foo@bar.com`, `qux queebly`, `grault`);
+  const expectedResult = `mailto:foo@bar.com?subject=qux%20queebly&amp;amp;body=grault`;
+  expect(result).toEqual(expectedResult);
 });
 
 describe("processes template", () => {
   test("with single element", () => {
     const value = `<div>foo</div>`;
     const result = processTemplate(value);
-    const expectedResult = "<div>foo</div>";
-    expectTemplateOutputToMatch(result, expectedResult);
+    expectTemplateOutputToMatch(result, value);
   });
 
   test("with single element and attribute", () => {
     const value = `<div style="color:red">foo</div>`;
     const result = processTemplate(value);
     const expectedResult = `<div style="color:red">foo</div>`;
+    expectTemplateOutputToMatch(result, expectedResult);
+  });
+
+  test("with mail URL", () => {
+    const mailURL = encodeMailUrl(`foo@bar.com`, `qux queebly`, `grault`);
+    const value = `<div><a href="mailto:foo@bar.com">foo</a></div>`;
+    const result = processTemplate(value);
+    const expectedResult = `<div><a href="mailto:foo@bar.com">foo</a></div>`;
+    expectTemplateOutputToMatch(result, expectedResult);
+  });
+
+  test("with encoded mail URL", () => {
+    const mailURL = encodeMailUrl(`foo@bar.com`, `qux queebly`, `grault`);
+    const value = `<div><a href="${mailURL}">foo</a></div>`;
+    const result = processTemplate(value);
+    const expectedResult = `<div><a href="mailto:foo@bar.com?subject=qux%20queebly&amp;amp;body=grault">foo</a></div>`;
     expectTemplateOutputToMatch(result, expectedResult);
   });
 
