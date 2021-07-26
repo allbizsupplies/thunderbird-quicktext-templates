1ecd7c1378c998753955d23db209085153c9390f
Brad Treloar <brad@treloardigital.com.au>
2021-07-26 13:03:22 +0930

    add lockdown collection instructions

--- src/templates/Print order notifications/OrderReadyToCollect.js
+++ src/templates/Print order notifications/OrderReadyToCollect.js
@@ -24,4 +24,12 @@ return template`
   <p>
     Your order is ready to collect. Please contact us if you need us to deliver it to you.
   </p>
+
+  <block style="background-color:#ffee4e;">
+    <p><strong>Collecting orders during COVID-19 lockdown</strong></p>
+    <ol>
+      <li>Call us once you arrive in the car park.</li>
+      <li>We will meet you at the front of the store and give you your order. </li>
+    </ol>
+  </block>
 `;
--- src/templates/Stat order notifications/OrderReadyToCollect.js
+++ src/templates/Stat order notifications/OrderReadyToCollect.js
@@ -20,4 +20,12 @@ return template`
   <p>
     Please contact us if you need us to deliver this order to you.
   </p>
+
+  <block style="background-color:#ffee4e;">
+    <p><strong>Collecting orders during COVID-19 lockdown</strong></p>
+    <ol>
+      <li>Call us once you arrive in the car park.</li>
+      <li>We will meet you at the front of the store and give you your order. </li>
+    </ol>
+  </block>
 `;
