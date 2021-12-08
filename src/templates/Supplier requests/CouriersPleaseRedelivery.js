74605056eb2692fd7242956b89c007d283857d99
Brad Treloar <brad.treloar@pm.me>
2021-12-08 13:27:17 +1030

    add couriers please redelivery template

---
+++ src/templates/Supplier requests/CouriersPleaseRedelivery.js
@@ -0,0 +1,24 @@
+/**
+ * name: Couriers Please: redeliver consignment
+ */
+
+const [consignmentID] = getInputs([{ label: "Consignment number" }]);
+
+setSubject(`Redeliver consignment`);
+
+return template`
+    <heading>
+        Redeliver consignment
+    </heading>
+
+    <block>
+        <p>Consignment ID: <strong>${consignmentID}</strong></p>
+    </block>
+
+    <p>
+        <strong>New delivery instructions:</strong>
+        <br />
+        <br />
+        <br />
+    </p>
+`;
